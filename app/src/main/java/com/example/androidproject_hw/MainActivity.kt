18f53268e1b9d1dd0df255558a22ae2dfd086a50
Svetttllana <Szgirovskaya@mail.ru>
2022-11-13 15:52:27 +0300

    Hw1;work with Activity and Intent-Filter

--- app/src/main/java/com/example/androidproject_hw/MainActivity.kt
+++ app/src/main/java/com/example/androidproject_hw/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.androidproject_hw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnGoToActivity2 = findViewById<Button>(R.id.btnGoToActivity2)
+        btnGoToActivity2.setOnClickListener {
+           startActivity(
+                Intent("android.intent.action.CHECKOUT_ACTIVITY2")
+            )
+        }
     }
+
 }
